cad64638b65ab322b47bdc848cb4f255dc5d556e
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-29 23:16:27 +0100

    Initial Commit

---
+++ SwiftUI Chat Application/SwiftUI_Chat_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chat_ApplicationApp.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chat_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
