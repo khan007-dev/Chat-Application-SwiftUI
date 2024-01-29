c1d80b5cccceb21f42a2658495f535ae4418f08e
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-29 23:38:02 +0100

    Login View

---
+++ SwiftUI Chat Application/View/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 29.01.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ SwiftUI Chat Application/View/Authentication/RegisterionView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterionView.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 29.01.2024.
+//
+
+import SwiftUI
+
+struct RegisterionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterionView()
+}
