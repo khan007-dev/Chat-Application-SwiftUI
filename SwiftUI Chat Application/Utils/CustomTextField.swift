89091fc7561e1251a629b59206de396ff3fd933b
abdulwariskhan <hellokhan.ch@gmail.com>
2024-01-30 23:25:46 +0100

    Building Login Screen

---
+++ SwiftUI Chat Application/Utils/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 30.01.2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}
