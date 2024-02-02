d7e9d858d59f515e4ef684bcbcc146b7d18854e5
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-02 10:43:41 +0100

    Edit Profile View

---
+++ SwiftUI Chat Application/View/Settings/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 02.02.2024.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
