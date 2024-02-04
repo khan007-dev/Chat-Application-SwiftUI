07bb0e303fcef6387005e8c2c041ba53beafd71a
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-04 10:53:31 +0100

    Status View Model

---
+++ SwiftUI Chat Application/View/Settings/StatusSelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  StatusSelectorView.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 04.02.2024.
+//
+
+import SwiftUI
+
+struct StatusSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusSelectorView()
+}
---
+++ SwiftUI Chat Application/ViewModel/StatusViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StatusViewModel.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 04.02.2024.
+//
+
+import Foundation
