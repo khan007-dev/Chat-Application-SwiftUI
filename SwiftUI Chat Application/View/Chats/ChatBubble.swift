5b580e0a49865c4c70acf57a6fe1815e2fc6f250
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-06 07:37:59 +0100

    Chat View UI

---
+++ SwiftUI Chat Application/View/Chats/ChatBubble.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBubble.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 06.02.2024.
+//
+
+import SwiftUI
+
+struct ChatBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBubble()
+}
