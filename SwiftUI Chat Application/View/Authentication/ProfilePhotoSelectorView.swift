e28b1662817fecdebb1fd16cf44093e891f1cb78
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-08 10:22:54 +0100

    Saved User data to cloud firestore

---
+++ SwiftUI Chat Application/View/Authentication/ProfilePhotoSelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePhotoSelectorView.swift
+//  SwiftUI Chat Application
+//
+//  Created by Khan on 08.02.2024.
+//
+
+import SwiftUI
+
+struct ProfilePhotoSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePhotoSelectorView()
+}
